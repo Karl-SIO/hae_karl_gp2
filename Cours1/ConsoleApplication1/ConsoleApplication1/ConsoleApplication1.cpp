59d18941619b4fc72abc02677e3096634d916c11
Karl <karl.merrienne@hotmail.fr>
2019-09-27 16:17:45 +0200

    3eme commit

--- Cours1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Cours1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,6 +5,7 @@
 //#include <iostream>
 #include <cstdlib> //#include <stdlib.h>
 #include <cstdio> //#include <stdio.h>
+#include <chrono> 
 
 /*
 int appel(int & s) {
@@ -13,11 +14,10 @@ int appel(int & s) {
 };
 */
 
-
 struct Vec3 {
-	float x;
-	float y;
-	float z;
+	int x;
+	int y;
+	int z;
 };
 
 Vec3 incrX(Vec3 _in) {
@@ -25,25 +25,36 @@ Vec3 incrX(Vec3 _in) {
 	return _in;
 }
 
+Vec3 stackOverflow(Vec3 _in) {
+	Vec3 temp = _in;
+	temp.y++;
+	return stackOverflow(temp);
+}
+
 int main()
 {
 	//Debug.Log(monobjet);
     //std::cout << "Hello World!\n"; 
 
-	/*
-	int foo = 0;
+	//int foo = 0;
 
-	const char * label = "sapin";
+	//const char * label = "sapin";
+
 	const char label2[6] = { 's', 'a', 'p', 'i', 'n', 0 };
 
+	const char * ptr = &label2[0];
+	ptr++;
+	printf("%c\n", *ptr);
+
+	/*
 	float v = 0.5f;
 	double ff = 0.5;
 
-	printf("Hello world this is a test : %d label : %s %f/n", foo, label, 66.66f);
-	printf("Hello world : l2 : %s/n", label2);
+	printf("Hello world this is a test : %d label : %s %f\n", foo, label, 66.66f);
+	printf("Hello world : l2 : %s\n", label2);
 
 	const char labelBug[5] = { 's', 'a', 'p', 'i', 'n' };
-	printf("Hello world : lb : %s/n", labelBug);
+	printf("Hello world : lb : %s\n", labelBug);
 	*/
 	
 	int x = 66;
@@ -53,6 +64,52 @@ int main()
 	Vec3 toto = { 1, 2, 3 }; // New Vec3()
 	incrX(toto);
 	printf("xval: %f\n", toto.x);
+
+	/*
+	Vec3 bob = { 1, 2, 3 };
+	bob = stackOverflow(bob);
+	printf("val x : %f\n", bob.x);
+	*/
+
+	Vec3 vecTab[3];
+	vecTab[0] = { 1, 2, 3 };
+	vecTab[1] = { 4, 5, 6 };
+	vecTab[2] = { 7, 8, 9 };
+	printf("v0x %f\n", vecTab[0].x);
+	vecTab[0].x++;
+	printf("v0x %f\n", vecTab[0]);
+
+	Vec3 * t0 = 0;
+	Vec3 * t1 = nullptr;
+	Vec3 * t2 = &vecTab[1];
+
+	//*(t2.y) = 555;
+	(*t2).y = 777;
+	t2->y = 888;
+
+	Vec3 * iter = &vecTab[0];
+	int i = 0;
+	for (i = 0; i < 3; ++i) {
+		printf("val avec x : %d\n", iter->x);
+		iter++;
+		//iter = iter + 1;
+		//iter = ( &vecTab[0] + 1 );
+	}
+
+	Vec3 * t3 = t2 + 1;
+	t2++;
+
+	auto start = std::chrono::system_clock::now();
+	int * bigBlock = (int*) malloc(1024 * 1024 * 1024);
+	for (int k = 0; k < 64 * 1024 * 1024; ++k) {
+		bigBlock[k] = 0xdeadbeef;
+	}
+	printf("beef ? : %x\n", bigBlock[1024 * 1024]);
+	auto end = std::chrono::system_clock::now();
+	auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	printf("time ? : %d\n", millis);
+
+	int _i = 0;
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
